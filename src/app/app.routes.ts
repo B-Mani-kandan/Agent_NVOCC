c7854c78fb4da3c29ae8f62eef46a9d14c2bad5a
B-Mani-kandan <baskarmanikandan48@gmail.com>
2025-02-20 15:24:48 +0530

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,39 +1,3 @@
-// import { Routes } from '@angular/router';
-// import { LoginComponent } from './components/login/login/login.component';
-// import { LayoutComponent } from './components/layout/layout/layout.component';
-// import { authGuard } from './guard/auth.guard';
-// import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
-// import { JobNvoccComponent } from './components/pages/job-nvocc/job-nvocc.component';
-
-// export const routes: Routes = [
-//   {
-//     path: '',
-//     redirectTo: 'login',
-//     pathMatch: 'full',
-//   },
-//   {
-//     path: 'login',
-//     component: LoginComponent,
-//     pathMatch: 'full',
-//   },
-//   {
-//     path: '',
-//     component: SidebarComponent,
-//     canActivate: [authGuard],
-//   },
-//   {
-//     path: 'dashboard',
-//     component: SidebarComponent,
-//     canActivate: [authGuard],
-//   },
-//   {
-//     path: 'nvocc-booking',
-//     component: JobNvoccComponent,
-//     canActivate: [authGuard],
-//     pathMatch: 'full',
-//   },
-// ];
-
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login/login.component';
 import { LayoutComponent } from './components/layout/layout/layout.component';
@@ -44,13 +8,11 @@ import { DashboardComponent } from './components/pages/dashboard/dashboard.compo
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
+    component: LoginComponent,
   },
   {
     path: 'login',
     component: LoginComponent,
-    pathMatch: 'full',
   },
   {
     path: '',
